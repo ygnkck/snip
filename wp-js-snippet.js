d9936e8c7c6c1eb95011fb38c1464e4cad305e32
ygnkck <75609918+ygnkck@users.noreply.github.com>
2021-03-17 12:29:23 +0530

    Update wp-js-snippet.js

--- wp-js-snippet.js
+++ wp-js-snippet.js
@@ -1,1 +1,25 @@
 
+
+/*** Jquery to Add sticky header wordpress ***/
+
+
+<script>
+jQuery( document ).on('scroll', function(){
+		if ( jQuery( document ).scrollTop() > 0 ){
+			jQuery( '.ms__main-header' ).addClass( 'fixed' );			
+		} else {
+			jQuery( '.ms__main-header' ).removeClass( 'fixed' );			
+		}
+	});
+</script>
+
+
+/*** jquery for mobile menu toggle ******/
+
+jQuery(document).ready(function($) {
+    jQuery("#mobilemenu").hide();
+    jQuery(".ms__menu-toggle").click(function() {
+        jQuery("#mobilemenu").slideToggle(500);
+		jQuery(".ms__mobile-menu").toggleClass('opened');
+    });
+});	
